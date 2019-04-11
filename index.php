b521b942e22ffebb0dee7ee367d61c25dab095e0
Sidónio Goenha <sid.goenha@gmail.com>
2019-04-11 22:01:49 +0200

    Login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Database\Util\Page;
 use \Database\Util\PageAdmin;
+use \Database\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -20,9 +21,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin(); 
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
